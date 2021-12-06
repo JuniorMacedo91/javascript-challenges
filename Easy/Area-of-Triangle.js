6b4dd0cf23b46e0f88f02b1f64d18d0e9392e108
Junior Macedo <80720405+JuniorMacedo91@users.noreply.github.com>
2021-12-06 00:22:09 -0300

    Delete Area-of-Triangle.js

--- Easy/Area-of-Triangle.js
+++
@@ -1,7 +0,0 @@
-// Write a function that takes the base and height of a triangle and return its area.
-
-function triangleArea(base, height){
-    return (base * height) / 2
-}
-
-triangleArea(7, 4)
