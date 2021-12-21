1cbae8aa52f94f1b4d655679b781f08450c44cbb
Junior Macedo <80720405+JuniorMacedo91@users.noreply.github.com>
2021-12-20 21:24:52 -0300

    Add files via upload

---
+++ Easy/10.Numbers Equal.js
@@ -0,0 +1,9 @@
+// Create a function that returns true when num1 is equal to num2; otherwise return false.
+
+function checkEqualnumber(num1, num2){
+    return (num1 === num2) ? true : false;
+}
+
+let result = checkEqualnumber(2,3)
+
+console.log(result)
